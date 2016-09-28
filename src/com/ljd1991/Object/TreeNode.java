dbc138b6450fab988a5a823dd78756101ff02124
LiuJida_PC <koalastorm@163.com>
2016-09-29 00:17:21 +0800

    initialization of repository by adding project files

---
+++ src/com/ljd1991/Object/TreeNode.java
@@ -0,0 +1,11 @@
+package com.ljd1991.Object;
+
+public class TreeNode {
+	public int value;
+	public TreeNode left;
+	public TreeNode right;
+	public TreeNode(int value) {
+		super();
+		this.value = value;
+	}	
+}
